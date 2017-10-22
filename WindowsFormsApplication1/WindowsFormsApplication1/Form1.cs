d51f229c7bac2982dae0ea8b94cda1c6a7aa8190
benjaporn01 <59030179@kmitl.ac.th>
2017-10-22 22:31:39 +0700

    unfinish

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -7,22 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Drawing.Drawing2D;
 
-namespace GDIPlus_1
+namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Paint(object sender, PaintEventArgs e)
-        {
-            Graphics g = e.Graphics;
-            HatchBrush brush;
-            int x = 20;
         }
     }
 }
