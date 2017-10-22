c90fba7cf72cf98d7ca49f3398b6cbddc00dbc27
benjaporn01 <59030179@kmitl.ac.th>
2017-10-22 22:45:26 +0700

    add hatchbrush

--- GDIPlus_1/GDIPlus_1/Form1.cs
+++ GDIPlus_1/GDIPlus_1/Form1.cs
@@ -20,9 +20,23 @@ namespace GDIPlus_1
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            this.SetClientSizeCore(500, 600);
             Graphics g = e.Graphics;
             HatchBrush brush;
             int x = 20;
+            int y = 20;
+            foreach (HatchStyle brushStyle in Enum.GetValues(typeof(HatchStyle)))
+            {
+                brush = new HatchBrush(brushStyle, Color.Navy, Color.Yellow);
+                g.FillRectangle(brush, x, y, 40, 20);
+                y += 30;
+                if ((y + 30) > this.ClientSize.Height)
+                {
+                    y = 20;
+                    x += 150;
+                }
+            }
+            g.Dispose();
         }
     }
 }
